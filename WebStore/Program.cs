e9ed48aac7690e01aa46eed29afb2103e556381e
Олег Нестеров <natol6@mail.ru>
2021-12-12 00:01:34 +0300

    В проект добавлены система маршрутизации и MVC

--- WebStore/Program.cs
+++ WebStore/Program.cs
@@ -1,7 +1,25 @@
 ﻿var builder = WebApplication.CreateBuilder(args);
+
+
+var services = builder.Services;
+services.AddControllersWithViews();
+
 var app = builder.Build();
-var configuration = app.Configuration;
-var greeting = configuration["CustomGreetings"];
-app.MapGet("/", () => greeting);
+//var configuration = app.Configuration;
+//var greeting = configuration["CustomGreetings"];
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+app.UseRouting();
+
+//app.MapGet("/", () => app.Configuration["CustomGreetings"]);
+app.MapGet("/throw", () =>
+{
+    throw new ApplicationException("Ошибка в программе!");
+});
+
+app.MapDefaultControllerRoute();
 
 app.Run();
