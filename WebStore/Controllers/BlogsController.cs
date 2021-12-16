794e1edc4e885d6fb923c6767500e95f409d18df
Олег Нестеров <natol6@mail.ru>
2021-12-16 13:26:07 +0300

    Добавлены блоги

---
+++ WebStore/Controllers/BlogsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebStore.Controllers
+{
+    public class BlogsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Blog()
+        {
+            return View();
+        }
+    }
+}
