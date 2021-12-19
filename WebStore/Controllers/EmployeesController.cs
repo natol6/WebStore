911cf14840c4db9fa37af664afd8fdcfb3a9cbe2
Олег Нестеров <natol6@mail.ru>
2021-12-19 23:44:26 +0300

    Тестовые данные сотрудников перенесены в отдельный класс TestData

--- WebStore/Controllers/EmployeesController.cs
+++ WebStore/Controllers/EmployeesController.cs
@@ -1,18 +1,17 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using WebStore.Models;
+using WebStore.Data;
 
 namespace WebStore.Controllers
 {
+    //[Route("Staff/{action=Index}/{Id?}")]
     public class EmployeesController : Controller
     {
-        private static readonly List<Employee> __Employees = new()
+        private readonly ICollection<Employee> __Employees;
+        public EmployeesController()
         {
-            new Employee { Id = 1, LastName = "Иванов", FirstName = "Иван", Patronymic = "Иванович", Age = 28, Position = "Старший продавец", DateOfEmployment = new DateOnly(2019, 6, 15) },
-            new Employee { Id = 2, LastName = "Петров", FirstName = "Петр", Patronymic = "Петрович", Age = 23, Position = "Главный бухгалтер", DateOfEmployment = new DateOnly(2018, 10, 28) },
-            new Employee { Id = 3, LastName = "Баширов", FirstName = "Руслан", Patronymic = "Михайлович", Age = 32, Position = "Директор", DateOfEmployment = new DateOnly(2017, 3, 3) },
-            new Employee { Id = 4, LastName = "Сидоров", FirstName = "Сидор", Patronymic = "Сидорович", Age = 18, Position = "Продавец", DateOfEmployment = new DateOnly(2019, 6, 15) },
-        };
-
+            __Employees = TestData.Employees;
+        }
         public IActionResult Index()
         {
             var result = __Employees;
---
+++ WebStore/Data/TestData.cs
@@ -0,0 +1,15 @@
+﻿using WebStore.Models;
+
+namespace WebStore.Data
+{
+    public static class TestData
+    {
+        public static List<Employee> Employees { get; } = new()
+        {
+            new Employee { Id = 1, LastName = "Иванов", FirstName = "Иван", Patronymic = "Иванович", Age = 28, Position = "Старший продавец", DateOfEmployment = new DateOnly(2019, 6, 15) },
+            new Employee { Id = 2, LastName = "Петров", FirstName = "Петр", Patronymic = "Петрович", Age = 23, Position = "Главный бухгалтер", DateOfEmployment = new DateOnly(2018, 10, 28) },
+            new Employee { Id = 3, LastName = "Баширов", FirstName = "Руслан", Patronymic = "Михайлович", Age = 32, Position = "Директор", DateOfEmployment = new DateOnly(2017, 3, 3) },
+            new Employee { Id = 4, LastName = "Сидоров", FirstName = "Сидор", Patronymic = "Сидорович", Age = 18, Position = "Продавец", DateOfEmployment = new DateOnly(2019, 6, 15) },
+        };
+    }
+}
