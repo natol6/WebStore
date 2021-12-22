6940b548a30824596fff467416b44ea9e1e5e80b
Олег Нестеров <natol6@mail.ru>
2021-12-22 16:06:07 +0300

    Добавлена проверка полей формы редактирования сотрудника с помощью регулярных выражений

--- WebStore/Data/TestData.cs
+++ WebStore/Data/TestData.cs
@@ -11,5 +11,6 @@ namespace WebStore.Data
             new EmployeeViewModel { Id = 3, LastName = "Баширов", FirstName = "Руслан", Patronymic = "Михайлович", Age = 32, Position = "Директор", DateOfEmployment = "03.03.2017" },
             new EmployeeViewModel { Id = 4, LastName = "Сидоров", FirstName = "Сидор", Patronymic = "Сидорович", Age = 18, Position = "Продавец", DateOfEmployment = "25.11.2020" },
         };
+
     }
 }
