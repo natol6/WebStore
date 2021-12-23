53130d5b3df06930eef7e13050a88c03d7b9deed
Олег Нестеров <natol6@mail.ru>
2021-12-23 21:44:01 +0300

    Добавление сущности и тестовых данных Product

--- WebStore/Services/Interfaces/IProductData.cs
+++ WebStore/Services/Interfaces/IProductData.cs
@@ -1,4 +1,6 @@
-﻿using WebStore.Domain.Entities;
+﻿using WebStore.Data;
+using WebStore.Domain;
+using WebStore.Domain.Entities;
 
 namespace WebStore.Services.Interfaces
 {
@@ -6,5 +8,14 @@ namespace WebStore.Services.Interfaces
     {
         IEnumerable<Section> GetSections();
         IEnumerable<Brand> GetBrands();
+        IEnumerable<Product> GetProducts(ProductFilter? Filter = null)
+        {
+            IEnumerable<Product> query = TestData.Products;
+            if(Filter?.SectionId != null)
+                query = query.Where(p => p.SectionId == Filter.SectionId);
+            if (Filter?.BrandId != null)
+                query = query.Where(p => p.BrandId == Filter.BrandId);
+            return query;
+        }
     }
 }
