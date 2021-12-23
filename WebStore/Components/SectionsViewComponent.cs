26b758dbf0f2f20bf511a4ad06a1d79e76f4fa0d
Олег Нестеров <natol6@mail.ru>
2021-12-23 10:22:08 +0300

    Созданы визуальные компоненты для брендов и секций. Создана модель представления секций.

---
+++ WebStore/Components/SectionsViewComponent.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebStore.Services.Interfaces;
+
+namespace WebStore.Components
+{
+    public class SectionsViewComponent : ViewComponent
+    {
+        private readonly IProductData _ProductData;
+        public SectionsViewComponent(IProductData productData) => _ProductData = productData;
+        public IViewComponentResult Invoke() => View();
+        
+    }
+}
