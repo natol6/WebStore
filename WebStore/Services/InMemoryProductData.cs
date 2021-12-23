d18aba1e61875e4afe699f59137369fc8ceb97fa
Олег Нестеров <natol6@mail.ru>
2021-12-23 08:28:07 +0300

    Добавление сервиса каталога товаров

---
+++ WebStore/Services/InMemoryProductData.cs
@@ -0,0 +1,20 @@
+﻿using WebStore.Domain.Entities;
+using WebStore.Services.Interfaces;
+using WebStore.Data;
+
+
+namespace WebStore.Services
+{
+    public class InMemoryProductData : IProductData
+    {
+        public IEnumerable<Brand> GetBrands()
+        {
+            return TestData.Brands;
+        }
+
+        public IEnumerable<Section> GetSections()
+        {
+            return TestData.Sections;
+        }
+    }
+}
