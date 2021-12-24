c2db9e0d5214b788f724fdfebcc71dd7b0335027
Олег Нестеров <natol6@mail.ru>
2021-12-24 10:44:16 +0300

    Создана инфраструктура для работы с товарами Контроллер калога товаров, модели представления каталога и товара и представление для каталога товаров

--- WebStore/Controllers/HomeController.cs
+++ WebStore/Controllers/HomeController.cs
@@ -1,12 +1,26 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebStore.Services.Interfaces;
+using WebStore.ViewModels;
 
 namespace WebStore.Controllers
 {
     public class HomeController : Controller
     {
        
-        public IActionResult Index()
+        public IActionResult Index([FromServices]IProductData productData)
         {
+            var products = productData
+                .GetProducts()
+                .OrderBy(p => p.Order)
+                .Take(6)
+                .Select(p => new ProductViewModel
+                {
+                    Id = p.Id,
+                    Name = p.Name,
+                    Price = p.Price,
+                    ImageUrl = p.ImageUrl,
+                });
+            ViewBag.Products = products;
             return View();
         }
         public void Throw(string message) => throw new ApplicationException(message);
