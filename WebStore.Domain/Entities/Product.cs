27cfb4d4cb660ced43b701238e70d3acd6b572c2
Олег Нестеров <natol6@mail.ru>
2021-12-28 13:18:26 +0300

    Добавлены аттрибуты для сущностей. Создан новый проект доступа к данным.

--- WebStore.Domain/Entities/Product.cs
+++ WebStore.Domain/Entities/Product.cs
@@ -1,19 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using WebStore.Domain.Entities.Base;
 using WebStore.Domain.Entities.Base.Interfaces;
+using Microsoft.EntityFrameworkCore;
 
 namespace WebStore.Domain.Entities
 {
+    [Index(nameof(Name))]
     public class Product : NamedEntity, IOrderedEntity
     {
         public int Order { get; set; }
         public int SectionId { get; set; }
+        [ForeignKey(nameof(SectionId))]
+        public Section Section { get; set; }
         public int? BrandId { get; set; }
+        [ForeignKey(nameof(BrandId))]
+        public Brand Brand { get; set; }
         public string ImageUrl { get; set; } 
+        [Column(TypeName = "decimal(18,2)")]
         public decimal Price { get; set; }
     }
 }
--- WebStore.Domain/Entities/Section.cs
+++ WebStore.Domain/Entities/Section.cs
@@ -1,11 +1,17 @@
 ﻿using WebStore.Domain.Entities.Base;
 using WebStore.Domain.Entities.Base.Interfaces;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
 
 namespace WebStore.Domain.Entities
 {
+    [Index(nameof(Name))]
     public class Section : NamedEntity, IOrderedEntity
     {
         public int Order { get; set; }
         public int? ParentId { get; set; }
+        [ForeignKey(nameof(ParentId))]
+        public Section Parent { get; set; }
+        public ICollection<Product> Products { get; set; }
     }
 }
