106e7b0e25881030c0acd361acfb02301d84f185
Олег Нестеров <natol6@mail.ru>
2022-01-20 23:46:24 +0300

    Базовый класс клиента WebAPI

---
+++ Services/WebStore.WebAPI.Clients/Base/BaseClient.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebStore.WebAPI.Clients.Base
+{
+    public abstract class BaseClient
+    {
+        protected HttpClient Http { get; }
+
+        protected string Address { get; }
+
+        protected BaseClient(HttpClient Client, string Address)
+        {
+            Http = Client;
+            this.Address = Address;
+        }
+    }
+}
