c7e672e56ad148f566bd3208290e1adcadcb0358
Олег Нестеров <natol6@mail.ru>
2022-01-21 10:29:07 +0300

    Визуальное управление клиентом тестового WebAPI

---
+++ UI/WebStore/Controllers/WebAPIController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebStore.Interfaces.TestAPI;
+
+namespace WebStore.Controllers
+{
+    public class WebAPIController : Controller
+    {
+        private readonly IValuesService _ValuesService;
+
+        public WebAPIController(IValuesService ValuesService) => _ValuesService = ValuesService;
+
+        public IActionResult Index()
+        {
+            var values = _ValuesService.GetValues();
+            return View(values);
+        }
+    }
+}
