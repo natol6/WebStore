0f58e064732cf7fec39612849a6eb382625ac0e0
Олег Нестеров <natol6@mail.ru>
2022-01-25 10:35:31 +0300

    Контроллер WebAPI для сотрудников реализован

--- Services/WebStoreWebAPI/Controllers/EmployeesApiController.cs
+++ Services/WebStoreWebAPI/Controllers/EmployeesApiController.cs
@@ -1,11 +1,56 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using WebStore.Domain.People;
+using WebStore.Interfaces.Services;
 
 namespace WebStoreWebAPI.Controllers
 {
-    [Route("api/[controller]")]
     [ApiController]
+    [Route("api/employees")]
     public class EmployeesApiController : ControllerBase
     {
+        private readonly IEmployeesData _EmployeesData;
+
+        public EmployeesApiController(IEmployeesData EmployeesData) => _EmployeesData = EmployeesData;
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var employees = _EmployeesData.GetAll();
+            return Ok(employees);
+        }
+
+        [HttpGet("{Id}")]
+        public IActionResult GetById(int Id)
+        {
+            var employee = _EmployeesData.GetById(Id);
+            if (employee is null)
+                return NotFound();
+
+            return Ok(employee);
+        }
+
+        [HttpPost]
+        public IActionResult Add(Employee employee)
+        {
+            var id = _EmployeesData.Add(employee);
+            return CreatedAtAction(nameof(GetById), new { Id = id }, employee);
+        }
+
+        [HttpPut]
+        public IActionResult Update(Employee employee)
+        {
+            var success = _EmployeesData.Edit(employee);
+            return Ok(success);
+        }
+
+        [HttpDelete("{Id}")]
+        public IActionResult Delete(int Id)
+        {
+            var result = _EmployeesData.Delete(Id);
+            return result
+                ? Ok(true)
+                : NotFound(false);
+        }
     }
 }
