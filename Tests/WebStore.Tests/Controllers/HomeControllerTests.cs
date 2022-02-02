e33ad6aff3fa851df3d4eb6e4513e96ca8338fe2
Олег Нестеров <natol6@mail.ru>
2022-02-02 19:07:37 +0300

    Метод тестирования с фейковой реализацией интерфейса

---
+++ Tests/WebStore.Tests/Controllers/HomeControllerTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System.Linq;
+using WebStore.Controllers;
+using WebStore.Domain;
+using WebStore.Domain.Entities;
+using WebStore.Interfaces.Services;
+using Assert = Xunit.Assert;
+
+namespace WebStore.Tests.Controllers
+{
+    [TestClass]
+    public class HomeControllerTests
+    {
+        [TestMethod]
+        public void Index_returns_View()
+        {
+            var product_data_mock = new Mock<IProductData>();
+            product_data_mock.Setup(s => s.GetProducts(It.IsAny<ProductFilter>()))
+               .Returns<ProductFilter>(f => Enumerable.Empty<Product>());
+
+            var controller = new HomeController();
+
+            var result = controller.Index(product_data_mock.Object);
+
+            Assert.IsType<ViewResult>(result);
+        }
+    }
+}
