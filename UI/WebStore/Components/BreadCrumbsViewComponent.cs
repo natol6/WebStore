1e3ea648582874f86c327aaa02c61d5ecb4a59ba
Олег Нестеров <natol6@mail.ru>
2022-02-08 18:07:46 +0300

    Корректировка хлебных крошек для коррктного отображения секций и брендов при выборе товара

--- UI/WebStore/Components/BreadCrumbsViewComponent.cs
+++ UI/WebStore/Components/BreadCrumbsViewComponent.cs
@@ -19,6 +19,7 @@ namespace WebStore.Components
         {
             var model = new BreadCrumbsViewModel();
 
+            
             if (int.TryParse(Request.Query["SectionId"], out var section_id))
             {
                 model.Section = _ProductData.GetSectionById(section_id);
@@ -30,7 +31,15 @@ namespace WebStore.Components
                 model.Brand = _ProductData.GetBrandById(brand_id);
 
             if (int.TryParse(Request.RouteValues["id"]?.ToString(), out var product_id))
-                model.Product = _ProductData.GetProductById(product_id)?.Name;
+            {
+                var SelectProduct = _ProductData.GetProductById(product_id);
+                model.Product = SelectProduct?.Name;
+                model.Brand = SelectProduct?.BrandId is not null ? _ProductData.GetBrandById((int)SelectProduct.BrandId) : null;
+                model.Section = _ProductData.GetSectionById(SelectProduct.SectionId);
+                if (model.Section?.ParentId is { } parent_section_id && model.Section.Parent is null)
+                    model.Section.Parent = _ProductData.GetSectionById(parent_section_id)!;
+            }
+                
 
             return View(model);
         }
