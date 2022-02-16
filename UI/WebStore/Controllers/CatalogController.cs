15ab58d2b5c2e181fb89b144e4cbc8d05c298f2a
Олег Нестеров <natol6@mail.ru>
2022-02-16 20:24:28 +0300

    Подготовка к написанию логики скрипта каталога товаров

--- UI/WebStore/Controllers/CatalogController.cs
+++ UI/WebStore/Controllers/CatalogController.cs
@@ -8,6 +8,8 @@ namespace WebStore.Controllers
 {
     public class CatalogController : Controller
     {
+        private const string __CatalogPageSize = "CatalogPageSize";
+
         private readonly IProductData _ProductData;
 
         private readonly IConfiguration _Configuration;
@@ -55,6 +57,25 @@ namespace WebStore.Controllers
                 return NotFound();
 
             return View(product.ToView());
+        }
+
+        public IActionResult GetProductsView(int? BrandId, int? SectionId, int Page = 1, int? PageSize = null)
+        {
+            var products = GetProducts(BrandId, SectionId, Page, PageSize);
+            return PartialView("Partial/_Products", products);
+        }
+
+        private IEnumerable<ProductViewModel> GetProducts(int? BrandId, int? SectionId, int Page, int? PageSize)
+        {
+            var products = _ProductData.GetProducts(new()
+            {
+                BrandId = BrandId,
+                SectionId = SectionId,
+                Page = Page,
+                PageSize = PageSize ?? _Configuration.GetValue(__CatalogPageSize, 6),
+            });
+
+            return products.Products.OrderBy(p => p.Order).ToView()!;
         }
     }
 }
